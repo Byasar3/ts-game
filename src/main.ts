25493cd87e8d80008676a8f71ead04414b0b5fa8
Byasar3 <bernayasar@hotmail.co.uk>
2024-04-07 17:16:53 +0100

    got the buttons to transition to different screens working

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,27 @@
 // entry point of application. 
 // import and initialise game
 // manage trasnsitions between difference screens
+
+// --- QUERY SELECTORS ---
+const loadingScreen = document.querySelector(".loading-screen");
+const selectionScreen = document.querySelector(".selection-screen");
+const gameScreen = document.querySelector(".game-screen");
+const loadingScreenEnterBtn = document.querySelector(".loading-screen__button");
+const selectionScreenBtn = document.querySelector(".selection-screen__button")
+
+
+
+// --- EVENT LISTENERS ---
+loadingScreenEnterBtn?.addEventListener("click", () => {
+	// hide loading screen
+	loadingScreen?.classList.add("hide");
+	// show selection screen
+	selectionScreen?.classList.add("show")
+})
+
+selectionScreenBtn?.addEventListener("click", () => {
+	// hide selection screen
+	selectionScreen?.classList.remove("show")
+	// show game screen
+	gameScreen?.classList.add("show")
+})
