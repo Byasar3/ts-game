15b74782b041d66f854d5c6a8089616b1b3dd4a7
Byasar3 <bernayasar@hotmail.co.uk>
2024-04-07 21:56:02 +0100

    deployment and got screen transition buttons working

--- src/main-game.ts
+++ src/main-game.ts
@@ -1,5 +1,5 @@
 // will contain main game logic:
 // player actions, enemy behaviour, scoring and other game play mechanics
 
-import { Character, player1 } from "./character";
-import { Enemy } from "./enemy";
+// import { Character, player1 } from "./character";
+// import { Enemy } from "./enemy";
