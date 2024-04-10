849cc3d2bdb9ab4c47a2e430db1f75c949d0f1ee
Byasar3 <bernayasar@hotmail.co.uk>
2024-04-10 16:30:16 +0100

    pseudocode for game logic

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { player1 } from "./character";
 import { levelOneEnemy, levelTwoEnemy, levelThreeEnemy, Enemy } from "./enemy";
-import "../styles/style.scss"
+import "../styles/style.scss";
 
 // --------------- QUERY SELECTORS ---------- //
 
@@ -11,9 +11,13 @@ const selectionScreen = document.querySelector(".selection-screen");
 const gameScreen = document.querySelector(".game-screen");
 const loadingScreenEnterBtn = document.querySelector(".loading-screen__button");
 const selectionScreenBtn = document.querySelector(".selection-screen__button");
-const characterHealth = document.querySelector(".character__stats__hp__amount");
-const characterStamina = document.querySelector(".character__stats__stamina__amount");
-const characterScore = document.querySelector(".character__stats__score__amount");
+const characterHealth = document.querySelector(".character__stats__hp");
+const characterStamina = document.querySelector(
+  ".character__stats__stamina"
+);
+const characterScore = document.querySelector(
+  ".character__stats__score"
+);
 
 // FOR SELECTION SCREEN:
 
@@ -37,8 +41,11 @@ const submitChoicesButton = document.querySelector<HTMLButtonElement>(
 
 const characterInfoDiv = document.querySelector(".character__info");
 const enemyHealth = document.querySelector(".enemy__stats__hp");
-const enemyImg = document.querySelector<HTMLImageElement>(".enemy__info__img")
-const enemyName = document.querySelector(".enemy__info__name")
+const enemyImg = document.querySelector<HTMLImageElement>(".enemy__info__img");
+const enemyName = document.querySelector(".enemy__info__name");
+const characterAttack = document.querySelector(".character__actions__atk");
+const characterRest = document.querySelector(".character__actions__rest");
+const characterHeal = document.querySelector(".character__actions__heal");
 
 // ---------------- GUARD CLAUSES ------------ //
 if (
@@ -57,19 +64,23 @@ if (
   !characterStamina ||
   !enemyHealth ||
   !enemyImg ||
-  !enemyName
+  !enemyName ||
+  !characterAttack ||
+  !characterRest ||
+  !characterHeal 
 ) {
   throw new Error("Issue with selectors");
 }
+
 // ---------------- VARIABLES ------------------//
 let currentCharacterHealth = player1.hp;
-characterHealth.innerHTML = `${currentCharacterHealth}`;
+characterHealth.innerHTML = `Health: ${currentCharacterHealth}`;
 
 let currentCharacterStamina = player1.stamina;
-characterStamina.innerHTML = `${currentCharacterStamina}`;
+characterStamina.innerHTML = `Stamina: ${currentCharacterStamina}`;
 
 let currentCharacterScore = player1.score;
-characterScore.innerHTML = `${currentCharacterScore}`;
+characterScore.innerHTML = `Score: ${currentCharacterScore}`;
 
 //note: might be able to do this by just characterThing.innerhtml = ${player1.thing}
 
@@ -83,7 +94,6 @@ enemyName.innerHTML = `${currentEnemyName}`;
 
 let currentEnemyImage = currentEnemy.img;
 enemyImg.src = currentEnemyImage;
-
 
 // ---------------- EVENT HANDLERS ----------- //
 
@@ -128,7 +138,31 @@ const handleNameInput = (event: Event) => {
   player1.name = target;
 };
 
+// FOR GAME SCREEN
 
+const handleCharacterAttack = () => {
+  // 1. a random number atk will be generated (outside function)
+  // 2. that number will be taken away from the enemy's hp, updating new hp
+  // 3. the attack stamina cost will be taken from user's stamina, updating new stamina
+  // 4. need to check if enemy is alive or not (outside function)
+isEnemyDefeated();
+  // 5. will trigger enemy attack (outside function)
+enemyAttack();
+}
+
+const handleCharacterRest = () => {
+  // 1. a random number rest will be generated (outside function)
+  // 2. that number will be added to character's stamina, updating new stamina
+  // 3. will trigger enemy attack (outside function)
+  enemyAttack();
+}
+
+const handleCharacterHeal = () => {
+  // 1. a random number heal will be generated (outside function)
+  // 2. that number will be added to characters health, updating new health
+  // 3. will trigger enemy attack (outside function)
+  enemyAttack();
+}
 // ---------------- FUNCTIONS ----------------- /
 
 // clicking through images
@@ -167,7 +201,35 @@ const loadGameScreen = () => {
 
 // game function:
 
+const enemyAttack = () => {
+  // 1. again random number generated
+  // 2. that random number is taken from character's health, updating new health
+}
 
+const isEnemyDefeated = () => {
+  // if statement to check if bollean isAlive is true 
+  // if false ie !isAlive:
+  // 1. give character a score (theres no function for this, maybe hard code a variable?)
+  // 2. give some sort of "wow you won, next level message"
+  // 3. set current enemy to next enemy
+  // else true = enemy is alive = game continues
+}
+
+const isCharacterDefeated = () => {
+  // if statement to check of boolean isAlive is true or false
+  // if false ie !isAlive:
+  // trigger game over screen (not currently created):
+  // need to add section into HTML -> classic game over, character in b&w? name, score, health, stamina, etc
+  // need to do the whole gamescreen.class list trigger thing -> set a function to loadEndGameLossScreen
+}
+
+const userReachesEndGame = () => {
+  // i guess will be same as isEnemyDefeated, but only for the remaining enemy?
+  // or when score reaches a determined point, as score at end game will be same for everyone
+  // trigger end screen winning
+  // same as above, needs HTML -> won, name, score, health, stamina etc
+  // again, need to do whole classlist.add etc
+}
 
 // ---------------- EVENT LISTENERS ---------- //
 
@@ -184,6 +246,14 @@ submitChoicesButton.addEventListener("click", handleUserInputs);
 leftArrow.addEventListener("click", handleLeftArrowClick);
 
 rightArrow.addEventListener("click", handleRightArrowClick);
+
+// FOR GAME SCREEN
+
+characterAttack.addEventListener("click", handleCharacterAttack);
+
+characterRest.addEventListener("click", handleCharacterRest);
+
+characterHeal.addEventListener("click", handleCharacterHeal);
 
 ////////////////// NOTES
 
