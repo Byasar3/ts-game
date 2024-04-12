c5ec1361e713125b0abfdd436d2c860209d808de
Byasar3 <bernayasar@hotmail.co.uk>
2024-04-12 12:36:44 +0100

    added more enemies to database

--- src/enemy.ts
+++ src/enemy.ts
@@ -1,6 +1,11 @@
-import monster1 from "../assets/procrastinatopus.png";
-import monster2 from "../assets/comparisoncrustaceans.png";
-import monster3 from "../assets/crappythoughtscloud.png";
+import procrastinatopus from "../assets/procrastinatopus.png";
+import comparisonCrustaceans from "../assets/comparisoncrustaceans.png";
+import crappyThoughtsCloud from "../assets/crappythoughtscloud.png";
+import lullabyLurker from "../assets/lullabylurker.png"
+import panicPhantom from "../assets/panicphantom.png"
+import gloomSlick from "../assets/gloomslick.png"
+import stubbornSkeleton from "../assets/stubbornskeleton.png"
+import perfectionismpixie from "../assets/perfectionismpixie.png"
 
 // creating enemy aliase/interface/object
 export type Enemy = {
@@ -15,16 +20,41 @@ export const enemyData: Enemy[] = [
   {
     name: "procrastinatopus",
     hp: 400,
-    img: monster1,
+    img: procrastinatopus,
   },
   {
     name: "comparison crustaceans",
     hp: 400,
-    img: monster2,
+    img: comparisonCrustaceans,
   },
   {
     name: "crappy thoughts cloud",
     hp: 400,
-    img: monster3,
+    img: crappyThoughtsCloud,
   },
+  {
+    name: "lullaby lurker",
+    hp: 400,
+    img: lullabyLurker
+  },
+  {
+    name: "panic phantom",
+    hp: 400,
+    img : panicPhantom
+  },
+  {
+    name: "gloom slick",
+    hp: 400,
+    img: gloomSlick
+  },
+  {
+    name: "stubborn skeleton",
+    hp: 400,
+    img: stubbornSkeleton
+  },
+  {
+    name: "perfectionism pixie",
+    hp: 400,
+    img: perfectionismpixie
+  }
 ];
